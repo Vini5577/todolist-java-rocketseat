df5b367137c1a232c847af8ccab6c3c2c0aaff79
Hugo Vinícius <ugopessoa@gmail.com>
2023-10-11 14:23:20 -0300

    inicio da aula3

--- src/main/java/br/com/hugovinicius/todolist/user/UserController.java
+++ src/main/java/br/com/hugovinicius/todolist/user/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
+
     @Autowired
     private IUserRepository userRepository;
 
@@ -19,9 +21,14 @@ public class UserController {
     public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
-        if(user != null) {
+        if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usúario já exite");
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
